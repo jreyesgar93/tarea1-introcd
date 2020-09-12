75fee41b41345297553ce0c5131c0270bf73c4d7
jreyesgar93 <50057293+jreyesgar93@users.noreply.github.com>
2020-09-12 12:10:25 -0500

    cambios en tariea

--- scrapping/tarea1_webscrapping.R
+++ scrapping/tarea1_webscrapping.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 library(rvest)
 library(magritrr )
 
@@ -87,3 +88,101 @@ ammount<-weather_cdmx %>%
 Juntar todas las variables en una sola base de datos y guardar resultado en csv
 
 '''     
+=======
+library(rvest)
+library(magritrr)
+
+
+'''
+Carga de la pagina que contiene los datos
+
+'''
+weather_cdmx <- read_html("https://www.timeanddate.com/weather/mexico/mexico-city/hourly")
+
+
+'''
+Datos a extraer: 
+
+Time
+Temp
+Weather
+Feels Like
+wind
+Humidity
+Chance rain 
+Ammount
+
+'''
+
+weather_scrapping<-function(html_name,td_pos){
+   
+       # Esta función toma tomo argumentos el html, 
+      #  la posición del elemento a extraer.
+        # Return Character vector
+      
+        vec<-c()
+        for (i in c(1:24)){
+                value<-html_name %>%
+                        html_node(xpath=paste('//*[@id="wt-hbh"]/tbody/tr[',i,']/td[',td_pos,']',sep="")) %>%
+                        html_text() %>%
+                        as.character()
+                vec<-c(vec,value)
+        }
+        return(vec)
+}
+
+
+time_scrapping<-function(html_name,th_pos){
+  
+  # Esta función es igual a la anterior pero para obtener el tiempo
+  
+  vec<-c()
+  for (i in c(1:24)){
+    value<-html_name %>%
+      html_node(xpath=paste('//*[@id="wt-hbh"]/tbody/tr[',i,']/th[',th_pos,']',sep="")) %>%
+      html_text() %>%
+      as.character()
+    vec<-c(vec,value)
+  }
+  return(vec)
+}
+
+time<-time_scrapping(weather_cdmx,1)
+
+temp<-weather_scrapping(weather_cdmx,2)
+
+weather<-weather_scrapping(weather_cdmx,3)
+
+
+feels_like<-weather_scrapping(weather_cdmx,4)
+
+
+wind<-weather_scrapping(weather_cdmx,5)
+                
+humidity<-weather_scrapping(weather_cdmx,7)
+
+
+chance_rain<-weather_cdmx %>%
+                html_nodes(xpath = '//td[(((count(preceding-sibling::*) + 1) = 9) and parent::*)]') %>%
+                html_text() %>%
+                sub("%", "", .) %>%
+                as.numeric() %>% 
+                divide_by(100)
+
+# otra forma de hacer lo mismo
+chance_rain2<-weather_cdmx %>%
+  html_node("td:nth-child(9)") %>%
+  html_text()
+as.numeric(sub("%","",chance_rain2))/100
+
+
+ammount<-weather_cdmx %>%
+            html_nodes(xpath = '//td[(((count(preceding-sibling::*) + 1) = 10) and parent::*)]') %>%
+            html_text() %>%
+            as.character()
+
+'''
+Juntar todas las variables en una sola base de datos y guardar resultado en csv
+
+'''     
+>>>>>>> 34a57b2de17c2e8f4312d697a9907cc85f89458a

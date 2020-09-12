217cac96d4b6a0bc135bd5af0b46499c60dde063
jreyesgar93 <50057293+jreyesgar93@users.noreply.github.com>
2020-09-12 17:37:06 -0500

    cambios scrapping

--- scrapping/tarea1_webscrapping.R
+++ scrapping/tarea1_webscrapping.R
@@ -1,95 +1,5 @@
 
 library(rvest)
-library(magritrr )
-
-
-'''
-Carga de la pagina que contiene los datos
-
-'''
-weather_cdmx <- read_html("https://www.timeanddate.com/weather/mexico/mexico-city/hourly")
-x<-1
-
-
-'''
-Datos a extraer: 
-
-Time
-Temp
-Weather
-Feels Like
-wind
-Humidity
-Chance rain 
-Ammount
-
-'''
-
-weather_scrapping<-function(html_name,td_pos){
-   
-       # Esta función toma tomo argumentos el html, 
-      #  la posición del elemento a extraer.
-        # Return Character vector
-      
-        vec<-c()
-        for (i in c(1:24)){
-                value<-html_name %>%
-                        html_node(xpath=paste('//*[@id="wt-hbh"]/tbody/tr[',i,']/td[',td_pos,']',sep="")) %>%
-                        html_text() %>%
-                        as.character()
-                vec<-c(vec,value)
-        }
-        return(vec)
-}
-
-
-
-
-
-time<-weather_cdmx %>%
-        html_node() %>%
-        html_text() %>%
-        as.numeric()
-
-
-temp<-weather_cdmx %>%
-        html_node() %>%
-        html_text() %>%
-        as.numeric()
-
-
-
-
-
-weather<-weather_cdmx %>%
-            html_node() %>%
-            html_text() %>%
-            as.character()
-
-
-feels_like<-weather_scrapping(weather_cdmx,4)
-
-
-wind<-weather_scrapping(weather_cdmx,5)
-                
-humidity<-weather_scrapping(weather_cdmx,7)
-
-chance_rain<-weather_cdmx %>%
-                html_node() %>%
-                html_text() %>%
-                as.numeric()
-
-ammount<-weather_cdmx %>%
-            html_node() %>%
-            html_text() %>%
-            as.numeric()
-
-'''
-Juntar todas las variables en una sola base de datos y guardar resultado en csv
-
-'''     
-=======
-library(rvest)
 library(magritrr)
 
 
@@ -167,13 +77,13 @@ chance_rain<-weather_cdmx %>%
                 html_text() %>%
                 sub("%", "", .) %>%
                 as.numeric() %>% 
-                divide_by(100)
+                (function(x)x/100)
 
 # otra forma de hacer lo mismo
-chance_rain2<-weather_cdmx %>%
-  html_node("td:nth-child(9)") %>%
-  html_text()
-as.numeric(sub("%","",chance_rain2))/100
+# chance_rain2<-weather_cdmx %>%
+#   html_node("td:nth-child(9)") %>%
+#   html_text()
+# as.numeric(sub("%","",chance_rain2))/100
 
 
 ammount<-weather_cdmx %>%
@@ -185,3 +95,4 @@ ammount<-weather_cdmx %>%
 Juntar todas las variables en una sola base de datos y guardar resultado en csv
 
 '''     
+write.csv(data.frame(time,temp,weather,chance_rain),"weather_cdmx.csv")
